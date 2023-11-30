2395f8108bd6bd494eba3c359b1a1cc223507072
Aryan Raj <aryanraj2k25@gmail.com>
2023-12-01 00:46:36 +0530

    server is now running

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 import { ApolloServer } from "@apollo/server";
-import {startStandAloneServer} from '@apollo/server/standalone'
-import {typeDefs} from './schema.js'
-//server setup 
+import { startStandaloneServer } from '@apollo/server/standalone'
 
+//db
+import _db from "./_db.js";
+
+//types
+import {typeDefs} from './schema.js'
+
+//resolvers
+const resolvers = {
+    Query: {
+      games() {
+        return db.games
+      },
+      authors() {
+        return db.authors
+      },
+      reviews() {
+        return db.reviews
+      },
+    }
+  }
+
+//server setup 
 const server= new ApolloServer({
     //now what we need to do is that we have to define the types which can be written as typeDefs 
     typeDefs,
-    //resolvers 
+    resolvers  
 });
 
-const {url}= await startStandAloneServer(server,{
+const {url}= await startStandaloneServer(server,{
     listen: {port: 4000}
 })
 
 console.log('server is ready at', 4000);
-
