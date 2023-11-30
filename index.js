34b14322b9bb37961ff05297eae2f594a67ff226
Aryan Raj <aryanraj2k25@gmail.com>
2023-12-01 00:05:17 +0530

    apollo server setup

---
+++ index.js
@@ -0,0 +1,16 @@
+import { ApolloServer } from "@apollo/server";
+import {startStandAloneServer} from '@apollo/server/standalone'
+
+//server setup 
+
+const server= new ApolloServer({
+    //now what we need to do is that we have to define the types which can be written as typeDefs 
+    //resolvers 
+});
+
+const {url}= await startStandAloneServer(server,{
+    listen: {port: 4000}
+})
+
+console.log('server is ready at', 4000);
+
